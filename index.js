d16849eb5c61d330ed190473478649fe37af8a88
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-06-29 19:13:28 +0530

    Connect mongoDB.

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const morgan = require("morgan");
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log("MongoDB is connected");
+});
+
 app.listen(8800, () => {
     console.log("Backend server is running");
 });
