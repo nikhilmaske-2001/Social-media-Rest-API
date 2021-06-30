8565216fbd83f99f3c1649739697896bd7d2d8dd
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-06-30 17:14:50 +0530

    Middleware and first request.

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 app.listen(8800, () => {
     console.log("Backend server is running");
 });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Homepage");
+});
+
+app.get("/users",(req, res) => {
+    res.send("Welcome to user page");
+});
