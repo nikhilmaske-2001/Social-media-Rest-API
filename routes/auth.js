2b3af9d95297008d80f0d427ae3930961c7f8944
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-03 19:43:36 +0530

    Created the dummy database.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("Hey its auth routes");
+router.get("/register", async (req, res) => {
+  const user = await new User({
+    username: "test1",
+    email: "test1@gmail.com",
+    password: "test1"
+  });
+
+  await user.save();
+  res.send("OK");
 });
 
 module.exports = router;
