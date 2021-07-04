02b0fb59b2fe2357b108886412e26e20cb90a862
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-04 17:45:12 +0530

    Secure the password by using bcrypt salt library.

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  });
-
   try {
+    // Hash the password so that it cannot be read from the db
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new User
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    });
+
+    // Save the user in the db
     const user = await newUser.save();
     res.send(200).json(user);
   } catch (error) {
