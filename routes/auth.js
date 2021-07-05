736bba49cd7c1f7a4f7222cd3177e174a1379afc
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-05 18:14:05 +0530

    Create login system.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+// Register system
 router.post("/register", async (req, res) => {
   try {
     // Hash the password so that it cannot be read from the db
@@ -19,7 +20,22 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.send(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// Login system
+router.post("/login", async (req, res)=> {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("User not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json("Wrong password");
+    
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
