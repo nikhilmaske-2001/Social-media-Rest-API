ed7889f0f585a8330216bfde1baf43045198fda4
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-06 17:49:19 +0530

    Add more schema in User schema.

--- routes/auth.js
+++ routes/auth.js
@@ -27,12 +27,15 @@ router.post("/register", async (req, res) => {
 // Login system
 router.post("/login", async (req, res)=> {
   try {
+    // Find the user email in the db
     const user = await User.findOne({email: req.body.email});
     !user && res.status(404).json("User not found");
 
+    // Compare the hashed password in the db
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     !validPassword && res.status(400).json("Wrong password");
     
+    // if the above user details matches with our db user then Successful
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
