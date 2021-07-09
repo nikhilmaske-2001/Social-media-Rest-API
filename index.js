d15bf58aa070e2b9bf7b9a8f4c28b689bf83b741
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-09 10:56:04 +0530

    Create a post

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(morgan("common"));
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running");
---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+// Create a post
+router.post("/", async (req, res) => {
+    // Create a new Post
+    const newPost = new Post(req.body);
+    try {
+        // Save the new Post
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
