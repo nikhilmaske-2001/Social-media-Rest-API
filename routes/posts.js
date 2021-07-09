aecf0cd44ee7ac50ae60db304e6290657c92a4f9
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-09 11:24:55 +0530

    Update a post.

--- routes/posts.js
+++ routes/posts.js
@@ -15,4 +15,21 @@ router.post("/", async (req, res) => {
     }
 });
 
+// Update a post
+router.put("/:id", async (req, res) => {
+    try {
+        // Verify if post is present in db
+        const post = await Post.findById(req.params.id);
+        // Verify its user post only
+        if(post.userId === req.body.userId) {
+            await post.updateOne({ $set: req.body });
+            res.status(200).json("Your post has been updated");
+        } else {
+            res.status(403).json("You can only update your post");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
