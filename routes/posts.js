fbecaa28e79eaa69c41f30169bd57e4947a74312
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-09 14:22:27 +0530

    delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -32,4 +32,19 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+// Delete a post
+router.delete("/:id", async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId) {
+            await post.deleteOne();
+            res.status(200).json("The post has been deleted");
+        } else {
+            res.status(403).json("You can only delete your own post");
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
