6151395d51eca231ccb1c1da57f335ddd3732ea4
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-07-26 15:17:56 +0530

    Change port for deployment.

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
     console.log("Backend server is running");
 });
